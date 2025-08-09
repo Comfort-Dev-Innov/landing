cd8c65875d0119b92388438307494eec31355bc3
Angelou Sereño <115084999+DanRed22@users.noreply.github.com>
2025-08-10 01:51:17 +0800

    Feature/UI fixes and patches (#5)
    * new changes
    
    * refactor: remove unused Image import from ServicesSection component

--- components/section/TechStackSection.tsx
+++ components/section/TechStackSection.tsx
@@ -7,8 +7,11 @@ import { StaticAuroraBackground } from '../ui/aurora-background';
 
 export default function TechStack() {
   return (
-    <StaticAuroraBackground>
-      <div id="tech-stack" className="flex flex-col gap-8 w-[92%] xl:w-6xl mx-auto bg-white/20 backdrop-blur-lg rounded-3xl py-32 border px-4 xl:px-0 border-white/30">
+    <StaticAuroraBackground className="h-fit flex py-8">
+      <div
+        id="tech-stack"
+        className="flex flex-col gap-8 w-[92%] xl:w-6xl mx-auto bg-white/20 backdrop-blur-lg rounded-3xl border px-4 py-8 xl:px-0 border-white/30"
+      >
         <div className="flex flex-col gap-2">
           <motion.div
             className="flex w-full justify-center drop-shadow-2xl"
@@ -17,10 +20,13 @@ export default function TechStack() {
             transition={{ duration: 0.6, delay: 0.2 }}
             viewport={{ once: true }}
           >
-            <h1 className="flex uppercase text-lg sm:text-xl tracking-[0.5em] min-[330px]:tracking-[0.8em] font-bold text-white drop-shadow-lg">Tech Stack</h1>
+            <h1 className="flex uppercase text-lg sm:text-3xl tracking-[0.5em] min-[330px]:tracking-[0.8em] font-bold text-white drop-shadow-lg">
+              Tech Stack
+            </h1>
           </motion.div>
           <p className="text-white text-center text-sm sm:text-lg text-shadow-md">
-            These are the following technologies we use to build projects at comfort.
+            These are the following technologies we use to build projects at
+            comfort.
           </p>
         </div>
         <div className="w-full flex flex-col gap-8">
@@ -32,9 +38,18 @@ export default function TechStack() {
             className="w-full flex justify-center items-center"
           >
             <Image
-              src="https://skillicons.dev/icons?i=androidstudio,apple,atom,aws,azure,bash,bootstrap,c,cs,cpp,cloudflare,codepen,css,devto,docker,eclipse,express,figma,firebase,flutter,gcp,git,github,githubactions,gitlab,gradle,html,java,js,jquery,laravel,linux,mongodb,mysql,nestjs,netlify,nextjs,nodejs,ps,php,electron,postgres,postman,powershell,py,react,redux,regex,sass,sequelize,sqlite,stackoverflow,svg,tailwind,tensorflow,twitter,ts,vercel,visualstudio,vite,vscode,webflow,webpack,windows,wordpress&perline=10"
+              src="https://skillicons.dev/icons?i=androidstudio,apple,atom,aws,azure,bash,bootstrap,c,cs,cpp,cloudflare,codepen,css,devto,docker,eclipse,express,figma,firebase,flutter,gcp,git,github,githubactions,gitlab,gradle,html,java,js,jquery,laravel,linux,mongodb,mysql,nestjs,netlify,nextjs,nodejs,ps,php,electron,postgres,postman,powershell,py,react,redux,regex,sass,sequelize,sqlite,stackoverflow,svg,tailwind,tensorflow,twitter,ts,vercel,visualstudio,vite,vscode,webflow,webpack,windows,wordpress&perline=12"
               alt="My Skills"
-              className="relative w-full h-full max-w-3xl"
+              className="hidden md:flex w-full h-full px-8"
+              width={500}
+              height={500}
+              unoptimized
+            />
+
+            <Image
+              src="https://skillicons.dev/icons?i=androidstudio,apple,atom,aws,azure,bash,bootstrap,c,cs,cpp,cloudflare,codepen,css,devto,docker,eclipse,express,figma,firebase,flutter,gcp,git,github,githubactions,gitlab,gradle,html,java,js,jquery,laravel,linux,mongodb,mysql,nestjs,netlify,nextjs,nodejs,ps,php,electron,postgres,postman,powershell,py,react,redux,regex,sass,sequelize,sqlite,stackoverflow,svg,tailwind,tensorflow,twitter,ts,vercel,visualstudio,vite,vscode,webflow,webpack,windows,wordpress&perline=4"
+              alt="My Skills"
+              className="relative md:hidden w-full h-full px-8"
               width={500}
               height={500}
               unoptimized
