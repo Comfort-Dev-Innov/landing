227e1f48a27f1593a87cf9012167fbd88f764e57
Katteu <katheamari@gmail.com>
2026-05-15 15:20:23 +0800

    feat: add About Us and Services sections to landing page
    - Introduced AboutUsSection and ServicesSection components for enhanced content organization.
    - Updated LandingPage to include new sections with a gradient background for improved visual appeal.
    - Modified Header to include 'about' and 'services' in LIGHT_SECTION_IDS for better navigation.

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,26 @@ import HeroSection from '@/components/section/HeroSection';
 import ProjectsSection from '@/components/section/ProjectsSection';
 import TechSection from '@/components/section/TechSection';
 import FormSection from '@/components/section/FormSection';
+import AboutUsSection from '@/components/section/AboutUsSection';
+import ServicesSection from '@/components/section/ServicesSection';
 import React from 'react';
 
 function LandingPage() {
   return (
       <>
         <HeroSection />
+        <div
+          className="flex flex-col px-[24px]"
+          style={{
+            background: `
+              linear-gradient(to top, #ffffff, #65C5F5),
+              linear-gradient(to right, rgba(125,208,244,0.2) 0%, rgba(81,188,244,0.2) 52%, rgba(143,216,245,0.2) 100%)
+            `
+          }}
+        >
+            <AboutUsSection />
+            <ServicesSection />
+        </div>
         <TechSection />
         <ProjectsSection />
         <FormSection />
---
+++ components/section/AboutUsSection.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AboutUsSection = () => {
+  return (
+    <section id="about" className="w-full bg-gradient-to-r from-[#F4FDFF] to-[#FFFFFF] rounded-t-[40px] px-[40px] py-[80px]">
+        TEST CONTENT
+    </section>
+  )
+}
+
+export default AboutUsSection
