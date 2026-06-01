a5def45d385454673aba82b320de80cf51853319
Katteu <katheamari@gmail.com>
2026-06-02 05:16:52 +0800

    feat: implement about us section

--- components/section/AboutUsSection.tsx
+++ components/section/AboutUsSection.tsx
@@ -3,7 +3,14 @@ import React from 'react'
 const AboutUsSection = () => {
   return (
     <section id="about" className="w-full bg-gradient-to-r from-[#F4FDFF] to-[#FFFFFF] rounded-t-[40px] px-[40px] py-[80px]">
-        TEST CONTENT
+        <div className="flex flex-col gap-[24px] w-full items-center">
+          <h1 className="font-poppins font-bold text-4xl text-black">
+            Comfort Dev is your <span className="text-primary italic">solution.</span>
+          </h1>
+          <p className="text-base font-inter text-black text-center">
+          At Comfort Dev, we&apos;re more than just a development team. We are your <br className="max-md:hidden"/><span className="font-bold text-primary">long-term partner </span>in building, scaling, and maintaining software solutions that drive business growth. <br/><br/>With a focus on clear communication, dependable delivery, and scalable architecture, we make <br className="max-md:hidden"/>outsourcing feel like an in-house experience.
+          </p>
+        </div>
     </section>
   )
 }
