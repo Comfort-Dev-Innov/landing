a3bcd2cb85c26740eaeb491ac8ce1957f1552e47
DanRed22 <angeloushatchitsolutions@gmail.com>
2026-06-09 08:56:10 +0800

    fix build error

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,5 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        'nunito-sans': ['var(--font-nunito-sans)', 'sans-serif'],
-        'inter': ['var(--font-inter)', 'sans-serif'],
-      },
-    },
-  },
-  plugins: [],
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
